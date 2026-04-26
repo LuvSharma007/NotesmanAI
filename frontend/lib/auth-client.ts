796f66d7bbb68d6a9e02f3ac7f5bf3ca53e2d0f4
baki <luvsharma9910@gmail.com>
2026-04-27 00:04:11 +0530

    update domain

--- frontend/lib/auth-client.ts
+++ frontend/lib/auth-client.ts
@@ -5,11 +5,11 @@ export const authClient = createAuthClient({
     plugins: [ 
         usernameClient(),
     ], 
-    baseURL: "http://187.127.156.129:4000",
+    baseURL: "https://notesman.in",
     trustedOrigins: [
 		// 'http://localhost:4000',
 		// 'http://api:4000',
-        "http://187.127.156.129:4000",
+        // "http://187.127.156.129:4000",
         "https://notesman.in"
     ],
 })
