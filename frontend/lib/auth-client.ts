b6fad32e4575998fff47e4f7264094e772ae4b6d
baki <luvsharma9910@gmail.com>
2026-05-15 14:02:37 +0530

    made dev and prod compatiable

--- frontend/lib/auth-client.ts
+++ frontend/lib/auth-client.ts
@@ -1,11 +1,17 @@
 import { createAuthClient } from "better-auth/react"  // Change this line
 import { usernameClient } from "better-auth/client/plugins"
 
+console.log("NODE_ENV in autheClient:",process.env.NODE_ENV);
+
+const appEnv = process.env.NEXT_PUBLIC_APP_ENV;
+console.log("APP_ENV in authClient:", appEnv);
+
+
 export const authClient = createAuthClient({
     plugins: [ 
         usernameClient(),
     ], 
-    baseURL: "/",
+    baseURL: process.env.NEXT_PUBLIC_APP_ENV === "production" ? "https:notesman.in" : "http://localhost:4000" ,
     trustedOrigins: [
 		'http://localhost:4000',
 		'http://api:4000',
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,10 +2,13 @@ import { NextResponse,NextRequest } from "next/server";
 export async function middleware(request:NextRequest){
     try {
         console.log("Middleware runned",);
-        console.log(process.env.NODE_ENV);
+        console.log("Middleware running, NODE_ENV:",process.env.NODE_ENV);
+
+        console.log("Middleware running, APP_ENV:", process.env.APP_ENV);
         
-        const cookie = request.cookies.get( process.env.NODE_ENV === "production" ? "__Secure-notesman.session_token" : "notesman.session_token");
-        
+        const cookie = request.cookies.get( process.env.NEXT_PUBLIC_APP_ENV === "production" ? "__Secure-notesman.session_token" : "notesman.session_token");
+        // const cookie = request.cookies.get("notesman.session_token");
+
         if(!cookie){
             console.log("Unauthorized Access");        
             return NextResponse.redirect(new URL("/login",request.url))
